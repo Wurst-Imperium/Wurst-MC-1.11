a4ac6018b46fd068be1a74e8d13d4e5c40fa6040
Alexander01998 <contact.wurstimperium@gmail.com>
2016-12-03 07:13:27 +0100

    Stop checking if the player moved off the path

--- src/tk/wurst_client/ai/FlyPathProcessor.java
+++ src/tk/wurst_client/ai/FlyPathProcessor.java
@@ -80,18 +80,6 @@ public class FlyPathProcessor extends PathProcessor
 		
 		lockControls();
 		
-		// check if player moved off the path
-		if(index > 0)
-		{
-			BlockPos prevPos = path.get(index - 1);
-			if((pos.getX() != prevPos.getX() && pos.getX() != nextPos.getX())
-				|| (pos.getY() != prevPos.getY()
-					&& pos.getY() != nextPos.getY())
-				|| (pos.getZ() != prevPos.getZ()
-					&& pos.getZ() != nextPos.getZ()))
-				System.err.println("Player moved off the path.");
-		}
-		
 		// move
 		BlockUtils.faceBlockClientHorizontally(nextPos);
 		
--- src/tk/wurst_client/ai/WalkPathProcessor.java
+++ src/tk/wurst_client/ai/WalkPathProcessor.java
@@ -42,18 +42,6 @@ public class WalkPathProcessor extends PathProcessor
 		
 		lockControls();
 		
-		// check if player moved off the path
-		if(index > 0)
-		{
-			BlockPos prevPos = path.get(index - 1);
-			if((pos.getX() != prevPos.getX() && pos.getX() != nextPos.getX())
-				|| (pos.getY() != prevPos.getY()
-					&& pos.getY() != nextPos.getY())
-				|| (pos.getZ() != prevPos.getZ()
-					&& pos.getZ() != nextPos.getZ()))
-				System.err.println("Player moved off the path.");
-		}
-		
 		// move
 		BlockUtils.faceBlockClientHorizontally(nextPos);
 		
