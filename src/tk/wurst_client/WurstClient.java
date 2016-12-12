c12e8cd9508eafcc9d77439c4a5acf69d1af2bbf
Alexander01998 <contact.wurstimperium@gmail.com>
2016-12-12 12:37:14 +0100

    Change version to 5.2

--- src/tk/wurst_client/WurstClient.java
+++ src/tk/wurst_client/WurstClient.java
@@ -26,7 +26,7 @@ public enum WurstClient
 {
 	INSTANCE;
 	
-	public static final String VERSION = "5.1";
+	public static final String VERSION = "5.2";
 	public static final String MINECRAFT_VERSION = "1.11";
 	public static final int PROTOCOL_VERSION = 315;
 	
