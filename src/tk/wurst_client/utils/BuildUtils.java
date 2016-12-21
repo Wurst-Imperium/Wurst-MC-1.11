05fa26b14e78d36a9849e174fd839c2039586738
Alexander01998 <contact.wurstimperium@gmail.com>
2016-12-21 11:40:16 +0100

    Deprecate BuildUtils

--- src/tk/wurst_client/utils/BuildUtils.java
+++ src/tk/wurst_client/utils/BuildUtils.java
@@ -14,6 +14,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 
+@Deprecated
 public class BuildUtils
 {
 	public static void build(int[][] building)
