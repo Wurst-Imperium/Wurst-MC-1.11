ff06aee7f05714e6b662956274254cf4af4a5863
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-01 16:57:55 +0100

    Update copyright year

--- src/tk/wurst_client/files/FileManager.java
+++ src/tk/wurst_client/files/FileManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.
+ * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -544,7 +544,7 @@ public class FileManager
 		try
 		{
 			String[] comment =
-				{"Copyright © 2014 - 2016 | Wurst-Imperium | All rights reserved.",
+				{"Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.",
 					"This Source Code Form is subject to the terms of the Mozilla Public",
 					"License, v. 2.0. If a copy of the MPL was not distributed with this",
 					"file, You can obtain one at http://mozilla.org/MPL/2.0/."};
