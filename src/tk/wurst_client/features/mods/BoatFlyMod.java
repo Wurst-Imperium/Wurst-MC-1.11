d96b2765e90e8d49250ac2be0cf1e04e35674b3c
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-03 14:42:52 +0100

    Fix copyright notice in BoatFlyMod

--- src/tk/wurst_client/features/mods/BoatFlyMod.java
+++ src/tk/wurst_client/features/mods/BoatFlyMod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016 | Alexander01998 | All rights reserved.
+ * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
