88270db9a8620d4679f1432deb49f8fcfc6fd646
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-04 16:31:08 +0100

    Fix AutoFish

--- src/tk/wurst_client/features/mods/AutoFishMod.java
+++ src/tk/wurst_client/features/mods/AutoFishMod.java
@@ -7,15 +7,22 @@
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.entity.projectile.EntityFishHook;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemFishingRod;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.play.server.SPacketSoundEffect;
+import tk.wurst_client.events.PacketInputEvent;
+import tk.wurst_client.events.listeners.PacketInputListener;
 import tk.wurst_client.events.listeners.UpdateListener;
+import tk.wurst_client.utils.InventoryUtils;
 
 @Mod.Info(description = "Automatically catches fish.",
 	name = "AutoFish",
 	tags = "FishBot, auto fish, fish bot, fishing",
 	help = "Mods/AutoFish")
 @Mod.Bypasses
-public class AutoFishMod extends Mod implements UpdateListener
+public class AutoFishMod extends Mod
+	implements UpdateListener, PacketInputListener
 {
 	private int timer;
 	
@@ -23,35 +30,61 @@ public class AutoFishMod extends Mod implements UpdateListener
 	public void onEnable()
 	{
 		wurst.events.add(UpdateListener.class, this);
+		wurst.events.add(PacketInputListener.class, this);
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		wurst.events.remove(UpdateListener.class, this);
+		wurst.events.remove(PacketInputListener.class, this);
 		timer = 0;
 	}
 	
 	@Override
 	public void onUpdate()
 	{
-		switch(timer)
+		// check timer
+		if(timer > 0)
 		{
-			case 0:
-				EntityFishHook hook = mc.player.fishEntity;
-				if(hook != null && hook.motionX == 0.0 && hook.motionZ == 0.0
-					&& hook.motionY <= -0.24)
-				{
-					mc.rightClickMouse();
-					timer = 20;
-				}
-				break;
-			
-			case 1:
-				mc.rightClickMouse();
-			default:
-				timer--;
-				break;
+			timer--;
+			return;
 		}
+		
+		// check bobber
+		if(mc.player.fishEntity != null)
+			return;
+		
+		rightClick();
+	}
+	
+	@Override
+	public void onReceivedPacket(PacketInputEvent event)
+	{
+		// check packet type
+		if(!(event.getPacket() instanceof SPacketSoundEffect))
+			return;
+		
+		// check sound type
+		if(((SPacketSoundEffect)event.getPacket())
+			.getSound() != SoundEvents.ENTITY_BOBBER_SPLASH)
+			return;
+		
+		rightClick();
+	}
+	
+	private void rightClick()
+	{
+		// check held item
+		ItemStack stack = mc.player.inventory.getCurrentItem();
+		if(InventoryUtils.isEmptySlot(stack)
+			|| !(stack.getItem() instanceof ItemFishingRod))
+			return;
+		
+		// right click
+		mc.rightClickMouse();
+		
+		// reset timer
+		timer = 15;
 	}
 }
