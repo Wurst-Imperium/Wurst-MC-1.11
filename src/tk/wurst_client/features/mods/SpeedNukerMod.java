f06c3abc83e0be9a92d69a81178b904ef9eb93d9
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-18 09:53:55 +0100

    Update SpeedNuker

--- src/tk/wurst_client/features/mods/SpeedNukerMod.java
+++ src/tk/wurst_client/features/mods/SpeedNukerMod.java
@@ -7,11 +7,7 @@
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.network.play.client.CPacketPlayerDigging;
-import net.minecraft.network.play.client.CPacketPlayerDigging.Action;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import tk.wurst_client.events.LeftClickEvent;
 import tk.wurst_client.events.listeners.LeftClickListener;
@@ -22,7 +18,7 @@ import tk.wurst_client.settings.ModeSetting;
 import tk.wurst_client.settings.SliderSetting;
 import tk.wurst_client.settings.SliderSetting.ValueDisplay;
 import tk.wurst_client.utils.BlockUtils;
-import tk.wurst_client.utils.ChatUtils;
+import tk.wurst_client.utils.BlockUtils.BlockValidator;
 
 @Mod.Info(description = "Faster Nuker that cannot bypass NoCheat+.",
 	name = "SpeedNuker",
@@ -31,13 +27,12 @@ import tk.wurst_client.utils.ChatUtils;
 @Mod.Bypasses(ghostMode = false,
 	latestNCP = false,
 	olderNCP = false,
-	antiCheat = false)
+	antiCheat = false,
+	mineplex = false)
 public class SpeedNukerMod extends Mod
 	implements LeftClickListener, UpdateListener
 {
-	private static Block currentBlock;
-	private BlockPos pos;
-	private int oldSlot = -1;
+	private BlockValidator validator;
 	
 	public CheckboxSetting useNuker =
 		new CheckboxSetting("Use Nuker settings", true)
@@ -60,7 +55,37 @@ public class SpeedNukerMod extends Mod
 	public final SliderSetting range =
 		new SliderSetting("Range", 6, 1, 6, 0.05, ValueDisplay.DECIMAL);
 	public final ModeSetting mode = new ModeSetting("Mode",
-		new String[]{"Normal", "ID", "Flat", "Smash"}, 0);
+		new String[]{"Normal", "ID", "Flat", "Smash"}, 0)
+	{
+		@Override
+		public void update()
+		{
+			switch(getSelected())
+			{
+				default:
+				case 0:
+					// normal mode
+					validator = (pos) -> true;
+					break;
+				
+				case 1:
+					// id mode
+					validator = (
+						pos) -> wurst.mods.nukerMod.id == BlockUtils.getId(pos);
+					break;
+				
+				case 2:
+					// flat mode
+					validator = (pos) -> pos.getY() >= mc.player.posY;
+					break;
+				
+				case 3:
+					// smash mode
+					validator = (pos) -> BlockUtils.getHardness(pos) >= 1;
+					break;
+			}
+		}
+	};
 	
 	@Override
 	public void initSettings()
@@ -88,19 +113,22 @@ public class SpeedNukerMod extends Mod
 	public Feature[] getSeeAlso()
 	{
 		return new Feature[]{wurst.mods.nukerMod, wurst.mods.nukerLegitMod,
-			wurst.mods.tunnellerMod, wurst.mods.fastBreakMod,
-			wurst.mods.autoMineMod};
+			wurst.mods.tunnellerMod, wurst.mods.kaboomMod,
+			wurst.mods.fastBreakMod, wurst.mods.autoMineMod};
 	}
 	
 	@Override
 	public void onEnable()
 	{
+		// disable other nukers
 		if(wurst.mods.nukerMod.isEnabled())
 			wurst.mods.nukerMod.setEnabled(false);
 		if(wurst.mods.nukerLegitMod.isEnabled())
 			wurst.mods.nukerLegitMod.setEnabled(false);
 		if(wurst.mods.tunnellerMod.isEnabled())
 			wurst.mods.tunnellerMod.setEnabled(false);
+		
+		// add listeners
 		wurst.events.add(LeftClickListener.class, this);
 		wurst.events.add(UpdateListener.class, this);
 	}
@@ -108,164 +136,53 @@ public class SpeedNukerMod extends Mod
 	@Override
 	public void onDisable()
 	{
+		// remove listeners
 		wurst.events.remove(LeftClickListener.class, this);
 		wurst.events.remove(UpdateListener.class, this);
-		if(oldSlot != -1)
-		{
-			mc.player.inventory.currentItem = oldSlot;
-			oldSlot = -1;
-		}
+		
+		// resets
 		wurst.mods.nukerMod.id = 0;
-		wurst.files.saveOptions();
 	}
 	
-	@SuppressWarnings("deprecation")
-	@Override
-	public void onUpdate()
-	{
-		if(mc.player.capabilities.isCreativeMode)
-		{
-			ChatUtils.error(getName() + " doesn't work in creative mode.");
-			setEnabled(false);
-			ChatUtils
-				.message("Switching to " + wurst.mods.nukerMod.getName() + ".");
-			wurst.mods.nukerMod.setEnabled(true);
-			return;
-		}
-		BlockPos newPos = find();
-		if(newPos == null)
-		{
-			if(oldSlot != -1)
-			{
-				mc.player.inventory.currentItem = oldSlot;
-				oldSlot = -1;
-			}
-			return;
-		}
-		pos = newPos;
-		currentBlock = mc.world.getBlockState(pos).getBlock();
-		if(wurst.mods.autoToolMod.isActive() && oldSlot == -1)
-			oldSlot = mc.player.inventory.currentItem;
-		if(!mc.player.capabilities.isCreativeMode
-			&& currentBlock.getPlayerRelativeBlockHardness(
-				mc.world.getBlockState(pos), mc.player, mc.world, pos) < 1)
-			wurst.mods.autoToolMod.setSlot(pos);
-		nukeAll();
-	}
-	
-	@SuppressWarnings("deprecation")
 	@Override
 	public void onLeftClick(LeftClickEvent event)
 	{
+		// check hitResult
 		if(mc.objectMouseOver == null
 			|| mc.objectMouseOver.getBlockPos() == null)
 			return;
-		if(mode.getSelected() == 1
-			&& mc.world.getBlockState(mc.objectMouseOver.getBlockPos())
-				.getBlock().getMaterial(null) != Material.AIR)
+		
+		// check mode
+		if(mode.getSelected() != 1)
+			return;
+		
+		// check material
+		if(BlockUtils
+			.getMaterial(mc.objectMouseOver.getBlockPos()) == Material.AIR)
+			return;
+		
+		// set id
+		wurst.mods.nukerMod.id =
+			BlockUtils.getId(mc.objectMouseOver.getBlockPos());
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		// abort if using IDNuker without an ID being set
+		if(mode.getSelected() == 1 && wurst.mods.nukerMod.id == 0)
+			return;
+		
+		for(BlockPos pos : BlockUtils.getValidBlocksByDistance(range.getValue(),
+			true, validator))
 		{
-			wurst.mods.nukerMod.id = Block.getIdFromBlock(mc.world
-				.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock());
-			wurst.files.saveOptions();
+			// AutoTool
+			wurst.mods.autoToolMod.setSlot(pos);
+			break;
 		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	private BlockPos find()
-	{
-		BlockPos closest = null;
-		float closestDistance = range.getValueF() + 1;
-		int nukerMode = mode.getSelected();
-		for(int y = (int)range.getValueF(); y >= (nukerMode == 2 ? 0
-			: -range.getValueF()); y--)
-			for(int x = (int)range.getValueF(); x >= -range.getValueF()
-				- 1; x--)
-				for(int z =
-					(int)range.getValueF(); z >= -range.getValueF(); z--)
-				{
-					if(mc.player == null)
-						continue;
-					if(x == 0 && y == -1 && z == 0)
-						continue;
-					int posX = (int)(Math.floor(mc.player.posX) + x);
-					int posY = (int)(Math.floor(mc.player.posY) + y);
-					int posZ = (int)(Math.floor(mc.player.posZ) + z);
-					BlockPos blockPos = new BlockPos(posX, posY, posZ);
-					Block block = mc.world.getBlockState(blockPos).getBlock();
-					float xDiff = (float)(mc.player.posX - posX);
-					float yDiff = (float)(mc.player.posY - posY);
-					float zDiff = (float)(mc.player.posZ - posZ);
-					float currentDistance =
-						BlockUtils.getBlockDistance(xDiff, yDiff, zDiff);
-					if(Block.getIdFromBlock(block) != 0 && posY >= 0
-						&& currentDistance <= range.getValueF())
-					{
-						if(nukerMode == 1 && Block
-							.getIdFromBlock(block) != wurst.mods.nukerMod.id)
-							continue;
-						if(nukerMode == 3
-							&& block.getPlayerRelativeBlockHardness(
-								mc.world.getBlockState(blockPos), mc.player,
-								mc.world, blockPos) < 1)
-							continue;
-						if(closest == null)
-						{
-							closest = blockPos;
-							closestDistance = currentDistance;
-						}else if(currentDistance < closestDistance)
-						{
-							closest = blockPos;
-							closestDistance = currentDistance;
-						}
-					}
-				}
-		return closest;
-	}
-	
-	@SuppressWarnings("deprecation")
-	private void nukeAll()
-	{
-		int nukerMode = mode.getSelected();
-		for(int y = (int)range.getValueF(); y >= (nukerMode == 2 ? 0
-			: -range.getValueF()); y--)
-			for(int x = (int)range.getValueF(); x >= -range.getValueF()
-				- 1; x--)
-				for(int z =
-					(int)range.getValueF(); z >= -range.getValueF(); z--)
-				{
-					int posX = (int)(Math.floor(mc.player.posX) + x);
-					int posY = (int)(Math.floor(mc.player.posY) + y);
-					int posZ = (int)(Math.floor(mc.player.posZ) + z);
-					if(x == 0 && y == -1 && z == 0)
-						continue;
-					BlockPos blockPos = new BlockPos(posX, posY, posZ);
-					Block block = mc.world.getBlockState(blockPos).getBlock();
-					float xDiff = (float)(mc.player.posX - posX);
-					float yDiff = (float)(mc.player.posY - posY);
-					float zDiff = (float)(mc.player.posZ - posZ);
-					float currentDistance =
-						BlockUtils.getBlockDistance(xDiff, yDiff, zDiff);
-					if(Block.getIdFromBlock(block) != 0 && posY >= 0
-						&& currentDistance <= range.getValueF())
-					{
-						if(nukerMode == 1 && Block
-							.getIdFromBlock(block) != wurst.mods.nukerMod.id)
-							continue;
-						if(nukerMode == 3
-							&& block.getPlayerRelativeBlockHardness(
-								mc.world.getBlockState(blockPos), mc.player,
-								mc.world, blockPos) < 1)
-							continue;
-						if(!mc.player.onGround)
-							continue;
-						EnumFacing side = mc.objectMouseOver.sideHit;
-						mc.player.connection.sendPacket(
-							new CPacketPlayerDigging(Action.START_DESTROY_BLOCK,
-								blockPos, side));
-						mc.player.connection.sendPacket(
-							new CPacketPlayerDigging(Action.STOP_DESTROY_BLOCK,
-								blockPos, side));
-					}
-				}
+		
+		// break all blocks
+		BlockUtils.getValidBlocks(range.getValue(), validator)
+			.forEach((pos) -> BlockUtils.breakBlockPacketSpam(pos));
 	}
 }
