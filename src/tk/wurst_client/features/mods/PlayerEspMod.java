d7a9561612baa9bb25ca58be3d14220c01b6591d
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-26 15:43:58 +0100

    Update PlayerESP

--- src/tk/wurst_client/features/mods/PlayerEspMod.java
+++ src/tk/wurst_client/features/mods/PlayerEspMod.java
@@ -7,8 +7,10 @@
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.entity.Entity;
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import tk.wurst_client.events.listeners.RenderListener;
 import tk.wurst_client.features.Feature;
 import tk.wurst_client.utils.RenderUtils;
@@ -20,6 +22,9 @@ import tk.wurst_client.utils.RenderUtils;
 @Mod.Bypasses
 public class PlayerEspMod extends Mod implements RenderListener
 {
+	private static final AxisAlignedBB PLAYER_BOX =
+		new AxisAlignedBB(-0.35, 0, -0.35, 0.35, 1.9, 0.35);
+	
 	@Override
 	public Feature[] getSeeAlso()
 	{
@@ -42,11 +47,52 @@ public class PlayerEspMod extends Mod implements RenderListener
 	@Override
 	public void onRender()
 	{
-		for(Object entity : mc.world.loadedEntityList)
-			if(entity instanceof EntityPlayer && !((Entity)entity).getName()
-				.equals(mc.getSession().getUsername()))
-				RenderUtils.entityESPBox((Entity)entity,
-					wurst.friends.contains(((EntityPlayer)entity).getName()) ? 1
-						: 0);
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		// draw boxes
+		for(EntityPlayer entity : mc.world.playerEntities)
+		{
+			if(entity == mc.player)
+				continue;
+			
+			// set color
+			if(wurst.friends.contains(entity.getName()))
+				GL11.glColor4f(0, 0, 1, 0.5F);
+			else
+			{
+				float factor = mc.player.getDistanceToEntity(entity) / 20F;
+				if(factor > 2)
+					factor = 2;
+				GL11.glColor4f(2 - factor, factor, 0, 0.5F);
+			}
+			
+			// set position
+			GL11.glPushMatrix();
+			GL11.glTranslated(entity.posX, entity.posY, entity.posZ);
+			
+			// draw box
+			RenderUtils.drawOutlinedBox(PLAYER_BOX);
+			
+			GL11.glPopMatrix();
+		}
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
