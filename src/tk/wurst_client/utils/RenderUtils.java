b03b0ff2c493c63da634500fbd698c39602f5271
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-26 20:11:33 +0100

    Remove RenderUtils.entityESPBox()

--- src/tk/wurst_client/utils/RenderUtils.java
+++ src/tk/wurst_client/utils/RenderUtils.java
@@ -17,9 +17,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
@@ -28,50 +26,6 @@ public class RenderUtils
 {
 	private static final AxisAlignedBB DEFAULT_AABB =
 		new AxisAlignedBB(0, 0, 0, 1, 1, 1);
-	
-	public static void entityESPBox(Entity entity, int mode)
-	{
-		GL11.glBlendFunc(770, 771);
-		GL11.glEnable(GL_BLEND);
-		GL11.glLineWidth(2.0F);
-		GL11.glDisable(GL11.GL_TEXTURE_2D);
-		GL11.glDisable(GL_DEPTH_TEST);
-		GL11.glDepthMask(false);
-		if(mode == 0)// Enemy
-			GL11.glColor4d(
-				1 - Minecraft.getMinecraft().player.getDistanceToEntity(entity)
-					/ 40,
-				Minecraft.getMinecraft().player.getDistanceToEntity(entity)
-					/ 40,
-				0, 0.5F);
-		else if(mode == 1)// Friend
-			GL11.glColor4d(0, 0, 1, 0.5F);
-		else if(mode == 2)// Other
-			GL11.glColor4d(1, 1, 0, 0.5F);
-		else if(mode == 3)// Target
-			GL11.glColor4d(1, 0, 0, 0.5F);
-		else if(mode == 4)// Team
-			GL11.glColor4d(0, 1, 0, 0.5F);
-		RenderManager renderManager =
-			Minecraft.getMinecraft().getRenderManager();
-		drawSelectionBoundingBox(new AxisAlignedBB(
-			entity.boundingBox.minX - 0.05 - entity.posX
-				+ (entity.posX - renderManager.renderPosX),
-			entity.boundingBox.minY - entity.posY
-				+ (entity.posY - renderManager.renderPosY),
-			entity.boundingBox.minZ - 0.05 - entity.posZ
-				+ (entity.posZ - renderManager.renderPosZ),
-			entity.boundingBox.maxX + 0.05 - entity.posX
-				+ (entity.posX - renderManager.renderPosX),
-			entity.boundingBox.maxY + 0.1 - entity.posY
-				+ (entity.posY - renderManager.renderPosY),
-			entity.boundingBox.maxZ + 0.05 - entity.posZ
-				+ (entity.posZ - renderManager.renderPosZ)));
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		GL11.glEnable(GL_DEPTH_TEST);
-		GL11.glDepthMask(true);
-		GL11.glDisable(GL_BLEND);
-	}
 	
 	public static void nukerBox(BlockPos blockPos, float damage)
 	{
