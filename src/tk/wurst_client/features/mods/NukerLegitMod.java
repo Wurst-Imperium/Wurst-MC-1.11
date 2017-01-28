ac4aebc31df2962433afc4502e7f6e96d757fd86
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-28 18:59:33 +0100

    Improve NukerLegit rendering

--- src/tk/wurst_client/features/mods/NukerLegitMod.java
+++ src/tk/wurst_client/features/mods/NukerLegitMod.java
@@ -7,6 +7,8 @@
  */
 package tk.wurst_client.features.mods;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
 import tk.wurst_client.events.LeftClickEvent;
@@ -207,12 +209,55 @@ public class NukerLegitMod extends Mod
 		if(currentBlock == null)
 			return;
 		
-		// check if block can be destroyed instantly
-		if(mc.player.capabilities.isCreativeMode
-			|| BlockUtils.getHardness(currentBlock) >= 1)
-			RenderUtils.nukerBox(currentBlock, 1);
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		// set position
+		GL11.glTranslated(currentBlock.getX(), currentBlock.getY(),
+			currentBlock.getZ());
+		
+		// get progress
+		float progress;
+		if(BlockUtils.getHardness(currentBlock) < 1)
+			progress = mc.playerController.curBlockDamageMP;
 		else
-			RenderUtils.nukerBox(currentBlock,
-				mc.playerController.curBlockDamageMP);
+			progress = 1;
+		
+		// set size
+		if(progress < 1)
+		{
+			GL11.glTranslated(0.5, 0.5, 0.5);
+			GL11.glScaled(progress, progress, progress);
+			GL11.glTranslated(-0.5, -0.5, -0.5);
+		}
+		
+		// get color
+		float red = progress * 2F;
+		float green = 2 - red;
+		
+		// draw box
+		GL11.glColor4f(red, green, 0, 0.25F);
+		RenderUtils.drawSolidBox();
+		GL11.glColor4f(red, green, 0, 0.5F);
+		RenderUtils.drawOutlinedBox();
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
