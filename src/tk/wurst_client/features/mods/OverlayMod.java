e3a3a735c3f5b75f175e1c6685c2717152b4376e
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-28 17:12:45 +0100

    Update Overlay

--- src/tk/wurst_client/features/mods/OverlayMod.java
+++ src/tk/wurst_client/features/mods/OverlayMod.java
@@ -7,9 +7,9 @@
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.block.Block;
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult.Type;
 import tk.wurst_client.events.listeners.RenderListener;
 import tk.wurst_client.features.Feature;
 import tk.wurst_client.utils.RenderUtils;
@@ -41,13 +41,58 @@ public class OverlayMod extends Mod implements RenderListener
 	@Override
 	public void onRender(float partialTicks)
 	{
-		if(mc.objectMouseOver == null
-			|| mc.objectMouseOver.typeOfHit != Type.BLOCK)
+		// check hitResult
+		if(mc.objectMouseOver == null)
 			return;
+		
+		// check position
 		BlockPos pos = mc.objectMouseOver.getBlockPos();
-		Block mouseOverBlock =
-			mc.world.getBlockState(mc.objectMouseOver.getBlockPos()).getBlock();
-		if(Block.getIdFromBlock(mouseOverBlock) != 0)
-			RenderUtils.nukerBox(pos, mc.playerController.curBlockDamageMP);
+		if(pos == null)
+			return;
+		
+		// check if hitting block
+		if(!mc.playerController.getIsHittingBlock())
+			return;
+		
+		// GL settings
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_LINE_SMOOTH);
+		GL11.glLineWidth(2);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		
+		GL11.glPushMatrix();
+		GL11.glTranslated(-mc.getRenderManager().renderPosX,
+			-mc.getRenderManager().renderPosY,
+			-mc.getRenderManager().renderPosZ);
+		
+		// set position
+		GL11.glTranslated(pos.getX(), pos.getY(), pos.getZ());
+		
+		// set size
+		GL11.glTranslated(0.5, 0.5, 0.5);
+		double v = mc.playerController.curBlockDamageMP;
+		GL11.glScaled(v, v, v);
+		GL11.glTranslated(-0.5, -0.5, -0.5);
+		
+		// get color
+		float red = mc.playerController.curBlockDamageMP * 2F;
+		float green = 2 - red;
+		
+		// draw box
+		GL11.glColor4f(red, green, 0, 0.25F);
+		RenderUtils.drawSolidBox();
+		GL11.glColor4f(red, green, 0, 0.5F);
+		RenderUtils.drawOutlinedBox();
+		
+		GL11.glPopMatrix();
+		
+		// GL resets
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_LINE_SMOOTH);
 	}
 }
