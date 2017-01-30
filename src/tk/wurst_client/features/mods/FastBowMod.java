67aec8105bdac8ed3f731658a112195a1af631ce
Alexander01998 <contact.wurstimperium@gmail.com>
2017-01-30 20:55:51 +0100

    Clean up FastBowMod

--- src/tk/wurst_client/features/mods/FastBowMod.java
+++ src/tk/wurst_client/features/mods/FastBowMod.java
@@ -1,22 +1,19 @@
 /*
  * Copyright © 2014 - 2017 | Wurst-Imperium | All rights reserved.
- *
+ * 
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package tk.wurst_client.features.mods;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.CPacketPlayer;
-import net.minecraft.network.play.client.CPacketPlayerDigging;
-import net.minecraft.network.play.client.CPacketPlayerDigging.Action;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import tk.wurst_client.events.listeners.UpdateListener;
 import tk.wurst_client.features.Feature;
+import tk.wurst_client.utils.InventoryUtils;
 
 @Mod.Info(
 	description = "Turns your bow into a machine gun.\n"
@@ -40,32 +37,38 @@ public class FastBowMod extends Mod implements UpdateListener
 	}
 	
 	@Override
-	public void onDisable()
+	public void onUpdate()
 	{
-		wurst.events.remove(UpdateListener.class, this);
+		// check if right-clicking
+		if(!mc.gameSettings.keyBindUseItem.pressed)
+			return;
+		
+		// check fly-kick
+		if(!mc.player.onGround && !mc.player.capabilities.isCreativeMode)
+			return;
+		
+		// check health
+		if(mc.player.getHealth() <= 0)
+			return;
+		
+		// check held item
+		ItemStack stack = mc.player.inventory.getCurrentItem();
+		if(InventoryUtils.isEmptySlot(stack)
+			|| !(stack.getItem() instanceof ItemBow))
+			return;
+		
+		mc.playerController.processRightClick(mc.player, mc.world,
+			EnumHand.MAIN_HAND);
+		
+		for(int i = 0; i < 20; i++)
+			mc.player.connection.sendPacket(new CPacketPlayer(false));
+		
+		mc.playerController.onStoppedUsingItem(mc.player);
 	}
 	
 	@Override
-	public void onUpdate()
+	public void onDisable()
 	{
-		if(mc.player.getHealth() > 0
-			&& (mc.player.onGround
-				|| Minecraft.getMinecraft().player.capabilities.isCreativeMode)
-			&& mc.player.inventory.getCurrentItem() != null
-			&& mc.player.inventory.getCurrentItem().getItem() instanceof ItemBow
-			&& mc.gameSettings.keyBindUseItem.pressed)
-		{
-			mc.playerController.processRightClick(mc.player, mc.world,
-				EnumHand.MAIN_HAND);
-			mc.player.inventory.getCurrentItem().getItem()
-				.onItemRightClick(mc.world, mc.player, EnumHand.MAIN_HAND);
-			for(int i = 0; i < 20; i++)
-				mc.player.connection.sendPacket(new CPacketPlayer(false));
-			Minecraft.getMinecraft().getConnection()
-				.sendPacket(new CPacketPlayerDigging(Action.RELEASE_USE_ITEM,
-					new BlockPos(0, 0, 0), EnumFacing.DOWN));
-			mc.player.inventory.getCurrentItem().getItem().onPlayerStoppedUsing(
-				mc.player.inventory.getCurrentItem(), mc.world, mc.player, 10);
-		}
+		wurst.events.remove(UpdateListener.class, this);
 	}
 }
