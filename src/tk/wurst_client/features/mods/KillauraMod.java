b7847ab4d3e66786e8d785cd42d0981fc9542a0e
Alexander01998 <contact.wurstimperium@gmail.com>
2017-02-07 08:43:20 +0100

    :gem: Clean up KillauraMod

--- src/tk/wurst_client/features/mods/KillauraMod.java
+++ src/tk/wurst_client/features/mods/KillauraMod.java
@@ -37,7 +37,7 @@ public class KillauraMod extends Mod implements UpdateListener
 			public void update()
 			{
 				speed.setDisabled(isChecked());
-			};
+			}
 		};
 	public SliderSetting speed =
 		new SliderSetting("Speed", 20, 0.1, 20, 0.1, ValueDisplay.DECIMAL);
@@ -78,7 +78,7 @@ public class KillauraMod extends Mod implements UpdateListener
 					teams.unlock();
 					teamColors.unlock();
 				}
-			};
+			}
 		};
 	public final CheckboxSetting players =
 		new CheckboxSetting("Attack players", true);
@@ -208,17 +208,13 @@ public class KillauraMod extends Mod implements UpdateListener
 	@Override
 	public void onEnable()
 	{
-		// TODO: Clean up this mess!
-		if(wurst.mods.killauraLegitMod.isEnabled())
-			wurst.mods.killauraLegitMod.setEnabled(false);
-		if(wurst.mods.multiAuraMod.isEnabled())
-			wurst.mods.multiAuraMod.setEnabled(false);
-		if(wurst.mods.clickAuraMod.isEnabled())
-			wurst.mods.clickAuraMod.setEnabled(false);
-		if(wurst.mods.tpAuraMod.isEnabled())
-			wurst.mods.tpAuraMod.setEnabled(false);
-		if(wurst.mods.triggerBotMod.isEnabled())
-			wurst.mods.triggerBotMod.setEnabled(false);
+		// disable other killauras
+		wurst.mods.killauraLegitMod.setEnabled(false);
+		wurst.mods.multiAuraMod.setEnabled(false);
+		wurst.mods.clickAuraMod.setEnabled(false);
+		wurst.mods.tpAuraMod.setEnabled(false);
+		wurst.mods.triggerBotMod.setEnabled(false);
+		
 		wurst.events.add(UpdateListener.class, this);
 	}
 	
