8b4bcde5067444bb6979429da6f9e9674bd1ea12
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-21 23:01:12 +0200

    Update to Minecraft 1.11.2

--- src/net/wurstclient/compatibility/WMinecraft.java
+++ src/net/wurstclient/compatibility/WMinecraft.java
@@ -12,7 +12,7 @@ import net.minecraft.client.network.NetHandlerPlayClient;
 public final class WMinecraft
 {
 	public static final String VERSION = "1.11";
-	public static final boolean REALMS = false;
+	public static final boolean REALMS = true;
 	public static final boolean COOLDOWN = true;
 	
 	public static final NavigableMap<Integer, String> PROTOCOLS;
