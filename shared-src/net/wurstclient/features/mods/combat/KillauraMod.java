a1ae42c7105d6ab9aa7cc9ef40993672d14e2529
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-10 10:24:12 +0200

    Update KillauraMod

--- shared-src/net/wurstclient/features/mods/combat/KillauraMod.java
+++ shared-src/net/wurstclient/features/mods/combat/KillauraMod.java
@@ -12,21 +12,17 @@ import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.compatibility.WPlayer;
 import net.wurstclient.events.listeners.UpdateListener;
 import net.wurstclient.features.Feature;
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
-import net.wurstclient.features.special_features.TargetSpf;
 import net.wurstclient.features.special_features.YesCheatSpf.Profile;
 import net.wurstclient.settings.CheckboxSetting;
-import net.wurstclient.settings.ColorsSetting;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 import net.wurstclient.utils.EntityUtils;
 import net.wurstclient.utils.EntityUtils.TargetSettings;
 import net.wurstclient.utils.RotationUtils;
 
-@SearchTags({"kill aura"})
-@HelpPage("Mods/Killaura")
+@SearchTags({"ForceField", "kill aura", "force field"})
 @Mod.Bypasses
 public final class KillauraMod extends Mod implements UpdateListener
 {
@@ -40,66 +36,13 @@ public final class KillauraMod extends Mod implements UpdateListener
 			}
 		};
 	public final SliderSetting speed =
-		new SliderSetting("Speed", 20, 0.1, 20, 0.1, ValueDisplay.DECIMAL);
+		new SliderSetting("Speed", 12, 0.1, 20, 0.1, ValueDisplay.DECIMAL);
 	public final SliderSetting range =
-		new SliderSetting("Range", 6, 1, 6, 0.05, ValueDisplay.DECIMAL);
+		new SliderSetting("Range", 4.25, 1, 6, 0.05, ValueDisplay.DECIMAL);
 	public final SliderSetting fov =
 		new SliderSetting("FOV", 360, 30, 360, 10, ValueDisplay.DEGREES);
 	public final CheckboxSetting hitThroughWalls =
 		new CheckboxSetting("Hit through walls", false);
-	
-	public final CheckboxSetting useTarget =
-		new CheckboxSetting("Use Target settings", true)
-		{
-			@Override
-			public void update()
-			{
-				if(isChecked())
-				{
-					TargetSpf target = wurst.special.targetSpf;
-					players.lock(target.players);
-					animals.lock(target.animals);
-					monsters.lock(target.monsters);
-					golems.lock(target.golems);
-					sleepingPlayers.lock(target.sleepingPlayers);
-					invisiblePlayers.lock(target.invisiblePlayers);
-					invisibleMobs.lock(target.invisibleMobs);
-					teams.lock(target.teams);
-					teamColors.lock(target.teamColors);
-				}else
-				{
-					players.unlock();
-					animals.unlock();
-					monsters.unlock();
-					golems.unlock();
-					sleepingPlayers.unlock();
-					invisiblePlayers.unlock();
-					invisibleMobs.unlock();
-					teams.unlock();
-					teamColors.unlock();
-				}
-			}
-		};
-	public final CheckboxSetting players =
-		new CheckboxSetting("Attack players", true);
-	public final CheckboxSetting animals =
-		new CheckboxSetting("Attack animals", true);
-	public final CheckboxSetting monsters =
-		new CheckboxSetting("Attack monsters", true);
-	public final CheckboxSetting golems =
-		new CheckboxSetting("Attack golems", true);
-	
-	public final CheckboxSetting sleepingPlayers =
-		new CheckboxSetting("Attack sleeping players", false);
-	public final CheckboxSetting invisiblePlayers =
-		new CheckboxSetting("Attack invisible players", false);
-	public final CheckboxSetting invisibleMobs =
-		new CheckboxSetting("Attack invisible mobs", false);
-	
-	public final CheckboxSetting teams = new CheckboxSetting("Teams", false);
-	public final ColorsSetting teamColors = new ColorsSetting("Team Colors",
-		new boolean[]{true, true, true, true, true, true, true, true, true,
-			true, true, true, true, true, true, true});
 	
 	private final TargetSettings targetSettings = new TargetSettings()
 	{
@@ -120,66 +63,11 @@ public final class KillauraMod extends Mod implements UpdateListener
 		{
 			return fov.getValueF();
 		}
-		
-		@Override
-		public boolean targetPlayers()
-		{
-			return players.isChecked();
-		}
-		
-		@Override
-		public boolean targetAnimals()
-		{
-			return animals.isChecked();
-		}
-		
-		@Override
-		public boolean targetMonsters()
-		{
-			return monsters.isChecked();
-		}
-		
-		@Override
-		public boolean targetGolems()
-		{
-			return golems.isChecked();
-		}
-		
-		@Override
-		public boolean targetSleepingPlayers()
-		{
-			return sleepingPlayers.isChecked();
-		}
-		
-		@Override
-		public boolean targetInvisiblePlayers()
-		{
-			return invisiblePlayers.isChecked();
-		}
-		
-		@Override
-		public boolean targetInvisibleMobs()
-		{
-			return invisibleMobs.isChecked();
-		}
-		
-		@Override
-		public boolean targetTeams()
-		{
-			return teams.isChecked();
-		}
-		
-		@Override
-		public boolean[] getTeamColors()
-		{
-			return teamColors.getSelected();
-		}
 	};
 	
 	public KillauraMod()
 	{
-		super("Killaura", "Automatically attacks entities around you.\n"
-			+ "Can be configured in various ways to attack only some entities and ignore others.");
+		super("Killaura", "Automatically attacks entities around you.");
 	}
 	
 	@Override
@@ -192,35 +80,24 @@ public final class KillauraMod extends Mod implements UpdateListener
 		settings.add(range);
 		settings.add(fov);
 		settings.add(hitThroughWalls);
-		
-		settings.add(useTarget);
-		settings.add(players);
-		settings.add(animals);
-		settings.add(monsters);
-		settings.add(golems);
-		settings.add(sleepingPlayers);
-		settings.add(invisiblePlayers);
-		settings.add(invisibleMobs);
-		settings.add(teams);
-		settings.add(teamColors);
 	}
 	
 	@Override
 	public Feature[] getSeeAlso()
 	{
-		return new Feature[]{wurst.special.targetSpf,
-			wurst.mods.killauraLegitMod, wurst.mods.multiAuraMod,
-			wurst.mods.clickAuraMod, wurst.mods.tpAuraMod,
-			wurst.mods.triggerBotMod, wurst.mods.criticalsMod};
+		return new Feature[]{wurst.special.targetSpf, wurst.special.yesCheatSpf,
+			wurst.mods.criticalsMod};
 	}
 	
 	@Override
 	public void onEnable()
 	{
 		// disable other killauras
+		wurst.mods.clickAuraMod.setEnabled(false);
+		wurst.mods.fightBotMod.setEnabled(false);
 		wurst.mods.killauraLegitMod.setEnabled(false);
 		wurst.mods.multiAuraMod.setEnabled(false);
-		wurst.mods.clickAuraMod.setEnabled(false);
+		wurst.mods.protectMod.setEnabled(false);
 		wurst.mods.tpAuraMod.setEnabled(false);
 		wurst.mods.triggerBotMod.setEnabled(false);
 		
@@ -249,14 +126,10 @@ public final class KillauraMod extends Mod implements UpdateListener
 		if(entity == null)
 			return;
 		
-		// prepare attack
+		// attack entity
 		EntityUtils.prepareAttack();
-		
-		// face entity
 		if(!RotationUtils.faceEntityPacket(entity))
 			return;
-		
-		// attack entity
 		EntityUtils.attackEntity(entity);
 		
 		// reset timer
@@ -273,21 +146,14 @@ public final class KillauraMod extends Mod implements UpdateListener
 			case MINEPLEX:
 			speed.resetUsableMax();
 			range.resetUsableMax();
-			hitThroughWalls.unlock();
 			break;
 			
 			case ANTICHEAT:
 			case OLDER_NCP:
 			case LATEST_NCP:
-			speed.setUsableMax(12);
-			range.setUsableMax(4.25);
-			hitThroughWalls.unlock();
-			break;
-			
 			case GHOST_MODE:
 			speed.setUsableMax(12);
 			range.setUsableMax(4.25);
-			hitThroughWalls.lock(() -> false);
 			break;
 		}
 	}
