7a9d814e1fd4fe5ae11b0d0a69ae5e9081ad2a66
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-11 11:54:58 +0200

    Update CriticalsMod

--- shared-src/net/wurstclient/features/mods/retro/CriticalsMod.java
+++ shared-src/net/wurstclient/features/mods/retro/CriticalsMod.java
@@ -7,7 +7,6 @@
  */
 package net.wurstclient.features.mods.retro;
 
-import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.wurstclient.compatibility.WConnection;
@@ -15,18 +14,16 @@ import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.LeftClickEvent;
 import net.wurstclient.events.listeners.LeftClickListener;
 import net.wurstclient.features.Feature;
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 import net.wurstclient.settings.ModeSetting;
 
 @SearchTags({"Crits"})
-@HelpPage("Mods/Criticals")
 @Mod.Bypasses(ghostMode = false)
 public final class CriticalsMod extends RetroMod implements LeftClickListener
 {
-	private int mode = 1;
-	private String[] modes = new String[]{"Jump", "Packet"};
+	private final ModeSetting mode =
+		new ModeSetting("Mode", new String[]{"Jump", "Packet"}, 1);
 	
 	public CriticalsMod()
 	{
@@ -36,14 +33,7 @@ public final class CriticalsMod extends RetroMod implements LeftClickListener
 	@Override
 	public void initSettings()
 	{
-		settings.add(new ModeSetting("Mode", modes, mode)
-		{
-			@Override
-			public void update()
-			{
-				mode = getSelected();
-			}
-		});
+		settings.add(mode);
 	}
 	
 	@Override
@@ -67,39 +57,46 @@ public final class CriticalsMod extends RetroMod implements LeftClickListener
 	@Override
 	public void onLeftClick(LeftClickEvent event)
 	{
-		if(mc.objectMouseOver != null
-			&& mc.objectMouseOver.entityHit instanceof EntityLivingBase)
-			doCritical();
+		if(mc.objectMouseOver == null
+			|| !(mc.objectMouseOver.entityHit instanceof EntityLivingBase))
+			return;
+		
+		doCritical();
 	}
 	
 	public void doCritical()
 	{
-		if(!wurst.mods.criticalsMod.isActive())
+		if(!isActive())
 			return;
-		if(!WMinecraft.getPlayer().isInWater()
-			&& !WMinecraft.getPlayer().isInsideOfMaterial(Material.LAVA)
-			&& WMinecraft.getPlayer().onGround)
-			switch(mode)
-			{
-				case 0:
-				WMinecraft.getPlayer().motionY = 0.1F;
-				WMinecraft.getPlayer().fallDistance = 0.1F;
-				WMinecraft.getPlayer().onGround = false;
-				break;
-				case 1:
-				double posX = WMinecraft.getPlayer().posX;
-				double posY = WMinecraft.getPlayer().posY;
-				double posZ = WMinecraft.getPlayer().posZ;
-				
-				WConnection.sendPacket(new CPacketPlayer.Position(posX,
-					posY + 0.0625D, posZ, true));
-				WConnection.sendPacket(
-					new CPacketPlayer.Position(posX, posY, posZ, false));
-				WConnection.sendPacket(new CPacketPlayer.Position(posX,
-					posY + 1.1E-5D, posZ, false));
-				WConnection.sendPacket(
-					new CPacketPlayer.Position(posX, posY, posZ, false));
-				break;
-			}
+		
+		if(!WMinecraft.getPlayer().onGround)
+			return;
+		
+		if(WMinecraft.getPlayer().isInWater()
+			|| WMinecraft.getPlayer().isInLava())
+			return;
+		
+		switch(mode.getSelected())
+		{
+			case 0:
+			WMinecraft.getPlayer().motionY = 0.1F;
+			WMinecraft.getPlayer().fallDistance = 0.1F;
+			WMinecraft.getPlayer().onGround = false;
+			break;
+			
+			case 1:
+			double posX = WMinecraft.getPlayer().posX;
+			double posY = WMinecraft.getPlayer().posY;
+			double posZ = WMinecraft.getPlayer().posZ;
+			WConnection.sendPacket(
+				new CPacketPlayer.Position(posX, posY + 0.0625D, posZ, true));
+			WConnection.sendPacket(
+				new CPacketPlayer.Position(posX, posY, posZ, false));
+			WConnection.sendPacket(
+				new CPacketPlayer.Position(posX, posY + 1.1E-5D, posZ, false));
+			WConnection.sendPacket(
+				new CPacketPlayer.Position(posX, posY, posZ, false));
+			break;
+		}
 	}
 }
