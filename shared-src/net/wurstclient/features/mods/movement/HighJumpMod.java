a088e6e61c4ec7f52d1a2ebf5a8a1c30f8014e73
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-18 12:31:50 +0200

    Update HighJumpMod

--- shared-src/net/wurstclient/features/mods/movement/HighJumpMod.java
+++ shared-src/net/wurstclient/features/mods/movement/HighJumpMod.java
@@ -5,16 +5,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.movement;
 
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"high jump"})
-@HelpPage("Mods/HighJump")
 @Mod.Bypasses(ghostMode = false,
 	latestNCP = false,
 	olderNCP = false,
@@ -22,17 +20,22 @@ import net.wurstclient.settings.SliderSetting.ValueDisplay;
 	mineplex = false)
 public final class HighJumpMod extends Mod
 {
-	public final SliderSetting height =
+	private final SliderSetting height =
 		new SliderSetting("Height", 6, 1, 100, 1, ValueDisplay.INTEGER);
 	
 	public HighJumpMod()
 	{
-		super("HighJump", "Makes you jump much higher.");
+		super("HighJump", "Allows you to jump higher.");
 	}
 	
 	@Override
 	public void initSettings()
 	{
 		settings.add(height);
+	}
+	
+	public double getAdditionalJumpMotion()
+	{
+		return isActive() ? height.getValue() * 0.1 : 0;
 	}
 }
