75eb6668dd209aa1601e28a936db1adc28b1b42d
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-20 14:29:04 +0200

    Update AntiBlindMod

--- shared-src/net/wurstclient/features/mods/render/AntiBlindMod.java
+++ shared-src/net/wurstclient/features/mods/render/AntiBlindMod.java
@@ -5,14 +5,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.render;
 
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 
-@SearchTags({"AntiBlindness", "anti blindness"})
-@HelpPage("Mods/AntiBlind")
+@SearchTags({"AntiBlindness", "NoBlindness", "anti blindness", "no blindness"})
 @Mod.Bypasses
 public final class AntiBlindMod extends Mod
 {
--- src/net/wurstclient/features/mods/ModManager.java
+++ src/net/wurstclient/features/mods/ModManager.java
@@ -28,6 +28,7 @@ import net.wurstclient.features.mods.items.KillPotionMod;
 import net.wurstclient.features.mods.items.TrollPotionMod;
 import net.wurstclient.features.mods.movement.*;
 import net.wurstclient.features.mods.other.AntiAfkMod;
+import net.wurstclient.features.mods.render.AntiBlindMod;
 import net.wurstclient.features.mods.retro.CriticalsMod;
 import net.wurstclient.features.mods.retro.FastBowMod;
 import net.wurstclient.features.mods.retro.FastEatMod;
