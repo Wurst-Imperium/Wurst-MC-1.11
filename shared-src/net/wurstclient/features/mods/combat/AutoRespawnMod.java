c4aba502d59cf05fe3cbaa42b3c1557ba2aea6f8
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-23 08:24:29 +0200

    Update AutoRespawnMod

--- shared-src/net/wurstclient/features/mods/combat/AutoRespawnMod.java
+++ shared-src/net/wurstclient/features/mods/combat/AutoRespawnMod.java
@@ -5,16 +5,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.combat;
 
 import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.listeners.DeathListener;
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 
 @SearchTags({"auto respawn"})
-@HelpPage("Mods/AutoRespawn")
 @Mod.Bypasses
 public final class AutoRespawnMod extends Mod implements DeathListener
 {
