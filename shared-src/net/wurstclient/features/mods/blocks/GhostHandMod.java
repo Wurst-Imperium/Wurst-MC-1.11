6e233a0750b0b5c20887570b4459ec07bd62fe9d
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-24 15:53:26 +0200

    Update GhostHandMod

--- shared-src/net/wurstclient/features/mods/blocks/GhostHandMod.java
+++ shared-src/net/wurstclient/features/mods/blocks/GhostHandMod.java
@@ -5,14 +5,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.blocks;
 
-import net.wurstclient.features.HelpPage;
+import net.wurstclient.features.Feature;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 
 @SearchTags({"ghost hand"})
-@HelpPage("Mods/GhostHand")
 @Mod.Bypasses(ghostMode = false)
 public final class GhostHandMod extends Mod
 {
@@ -20,13 +19,18 @@ public final class GhostHandMod extends Mod
 	{
 		super("GhostHand",
 			"Allows you to reach specific blocks through walls.\n"
-				+ "Use .ghosthand id <block id> or .ghosthand name <block name>\n"
-				+ "to specify it.");
+				+ "Type §l.ghosthand id <block_id>§r or §l.ghosthand name <block_name>§r to specify it.");
 	}
 	
 	@Override
 	public String getRenderName()
 	{
 		return getName() + " [" + wurst.options.ghostHandID + "]";
+	}
+	
+	@Override
+	public Feature[] getSeeAlso()
+	{
+		return new Feature[]{wurst.commands.ghostHandCmd};
 	}
 }
