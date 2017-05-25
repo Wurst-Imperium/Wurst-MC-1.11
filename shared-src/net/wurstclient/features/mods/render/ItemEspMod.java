6116513767a1658600a5363eca61bf6fb0d91e1b
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-25 12:09:39 +0200

    Remove ItemEspMod

--- shared-src/net/wurstclient/features/mods/render/ItemEspMod.java
+++ shared-src/net/wurstclient/features/mods/render/ItemEspMod.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.render;
 
 import org.lwjgl.opengl.GL11;
 
@@ -15,13 +15,11 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.listeners.RenderListener;
 import net.wurstclient.features.Feature;
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 import net.wurstclient.utils.RenderUtils;
 
 @SearchTags({"item esp"})
-@HelpPage("Mods/ItemESP")
 @Mod.Bypasses
 public final class ItemEspMod extends Mod implements RenderListener
 {
