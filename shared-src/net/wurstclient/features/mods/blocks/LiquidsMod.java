c9b3d6a525d8f7bbe1be48f17da5898683c650f7
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-25 15:35:12 +0200

    Update LiquidsMod

--- shared-src/net/wurstclient/features/mods/blocks/LiquidsMod.java
+++ shared-src/net/wurstclient/features/mods/blocks/LiquidsMod.java
@@ -5,17 +5,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.blocks;
 
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 
-@HelpPage("Mods/Liquids")
 @Mod.Bypasses(ghostMode = false, latestNCP = false, olderNCP = false)
 public final class LiquidsMod extends Mod
 {
 	public LiquidsMod()
 	{
-		super("Liquids", "Allows you to interact with liquid blocks.");
+		super("Liquids",
+			"Allows you to interact with liquid blocks as if they were solid.");
 	}
 }
