19588d0613baff8761633b17745ea8a1f7a103c7
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-29 16:26:26 +0200

    Update TrueSightMod

--- shared-src/net/wurstclient/features/mods/render/TrueSightMod.java
+++ shared-src/net/wurstclient/features/mods/render/TrueSightMod.java
@@ -5,14 +5,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.render;
 
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 
 @SearchTags({"true sight"})
-@HelpPage("Mods/TrueSight")
 @Mod.Bypasses
 public final class TrueSightMod extends Mod
 {
