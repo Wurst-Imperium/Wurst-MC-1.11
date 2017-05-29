7da56641cdef60bb8b4a23414bf931dc672b2ead
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-29 13:11:16 +0200

    Update PlayerEspMod

--- shared-src/net/wurstclient/features/mods/render/PlayerEspMod.java
+++ shared-src/net/wurstclient/features/mods/render/PlayerEspMod.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.render;
 
 import org.lwjgl.opengl.GL11;
 
@@ -14,14 +14,12 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.wurstclient.compatibility.WMinecraft;
 import net.wurstclient.events.listeners.RenderListener;
 import net.wurstclient.features.Feature;
-import net.wurstclient.features.HelpPage;
 import net.wurstclient.features.Mod;
 import net.wurstclient.features.SearchTags;
 import net.wurstclient.utils.EntityFakePlayer;
 import net.wurstclient.utils.RenderUtils;
 
 @SearchTags({"player esp"})
-@HelpPage("Mods/PlayerESP")
 @Mod.Bypasses
 public final class PlayerEspMod extends Mod implements RenderListener
 {
@@ -36,8 +34,8 @@ public final class PlayerEspMod extends Mod implements RenderListener
 	@Override
 	public Feature[] getSeeAlso()
 	{
-		return new Feature[]{wurst.mods.tracersMod, wurst.mods.playerFinderMod,
-			wurst.mods.mobEspMod, wurst.mods.prophuntEspMod};
+		return new Feature[]{wurst.mods.tracersMod, wurst.mods.mobEspMod,
+			wurst.mods.itemEspMod};
 	}
 	
 	@Override
