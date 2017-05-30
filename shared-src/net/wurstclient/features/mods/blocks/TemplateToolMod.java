01d4cff0075c4324921c30b30703bf8e8d4788e6
Alexander01998 <contact.wurstimperium@gmail.com>
2017-05-30 15:13:48 +0200

    Update TemplateToolMod

--- shared-src/net/wurstclient/features/mods/blocks/TemplateToolMod.java
+++ shared-src/net/wurstclient/features/mods/blocks/TemplateToolMod.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package net.wurstclient.features.mods;
+package net.wurstclient.features.mods.blocks;
 
 import java.io.File;
 import java.io.FileWriter;
